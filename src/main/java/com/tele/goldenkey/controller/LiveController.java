1f5c19adf14c242aa42c5d0d2e4cae71f904a777
lihongbin <836559525@qq.com>
2021-03-21 08:40:24 +0800

    live  ok

--- src/main/java/com/tele/goldenkey/controller/LiveController.java
+++ src/main/java/com/tele/goldenkey/controller/LiveController.java
@@ -11,6 +11,7 @@ import com.tele.goldenkey.util.ValidateUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +19,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
 import java.util.HashMap;
 
 
@@ -26,15 +26,14 @@ import java.util.HashMap;
 @RestController
 @RequestMapping("/live")
 @Slf4j
+@RequiredArgsConstructor
 public class LiveController extends BaseController {
-    @Resource
-    private LiveService liveService;
-    @Resource
-    private ApplicationContext applicationContext;
+    private final LiveService liveService;
+    private final ApplicationContext applicationContext;
 
     @ApiOperation(value = "获取直播流推送地址")
     @PostMapping(value = "/get/push-url/{id}")
-    public APIResult<HashMap> getPushUrl(
+    public APIResult<HashMap<String, String>> getPushUrl(
             @ApiParam(name = "id", value = "id", required = true, type = "String")
             @PathVariable("id") String encodeGroupId) throws ServiceException {
         Integer id = N3d.decode(encodeGroupId);
@@ -44,36 +43,36 @@ public class LiveController extends BaseController {
         applicationContext.publishEvent(new OpenLiveEvent(getCurrentUserId(), id));
         HashMap<String, String> hashMap = Maps.newHashMap();
         hashMap.put("pushUrl", pushUrl);
-        return APIResultWrap.ok();
+        return APIResultWrap.ok(hashMap);
     }
 
     @ApiOperation(value = "是否开播")
     @PostMapping(value = "/is-open/{id}")
-    public APIResult<HashMap> isOpen(@ApiParam(name = "id", value = "id", required = true, type = "String")
-                                     @PathVariable("id") String encodeGroupId) throws ServiceException {
+    public APIResult<HashMap<String, Boolean>> isOpen(@ApiParam(name = "id", value = "id", required = true, type = "String")
+                                                      @PathVariable("id") String encodeGroupId) throws ServiceException {
         Integer id = N3d.decode(encodeGroupId);
         HashMap<String, Boolean> hashMap = Maps.newHashMap();
         hashMap.put("isOpen", liveService.isOpen(id));
-        return APIResultWrap.ok();
+        return APIResultWrap.ok(hashMap);
     }
 
     @ApiOperation(value = "关闭直播")
     @PostMapping(value = "/close/{id}")
-    public APIResult<HashMap> close(@ApiParam(name = "id", value = "id", required = true, type = "String")
-                                    @PathVariable("id") String encodeGroupId) throws ServiceException {
+    public APIResult<HashMap<String, Boolean>> close(@ApiParam(name = "id", value = "id", required = true, type = "String")
+                                                     @PathVariable("id") String encodeGroupId) throws ServiceException {
         Integer id = N3d.decode(encodeGroupId);
         HashMap<String, Boolean> hashMap = Maps.newHashMap();
         hashMap.put("close", liveService.close(id));
-        return APIResultWrap.ok();
+        return APIResultWrap.ok(hashMap);
     }
 
     @ApiOperation(value = "获取直播地址")
     @PostMapping(value = "/get/live-url/{id}")
-    public APIResult<HashMap> getLiveUrl(@ApiParam(name = "id", value = "id", required = true, type = "String")
-                                         @PathVariable("id") String encodeGroupId) throws ServiceException {
+    public APIResult<HashMap<String, String>> getLiveUrl(@ApiParam(name = "id", value = "id", required = true, type = "String")
+                                                         @PathVariable("id") String encodeGroupId) throws ServiceException {
         Integer id = N3d.decode(encodeGroupId);
         HashMap<String, String> hashMap = Maps.newHashMap();
         hashMap.put("liveUrl", liveService.getLiveUrl(id));
-        return APIResultWrap.ok();
+        return APIResultWrap.ok(hashMap);
     }
 }
