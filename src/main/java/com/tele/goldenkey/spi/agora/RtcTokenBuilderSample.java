c72bf26635904a1c05907e485afa2c4c75002360
lihongbin <836559525@qq.com>
2021-03-29 14:02:24 +0800

    add cache

--- src/main/java/com/tele/goldenkey/spi/agora/RtcTokenBuilderSample.java
+++ src/main/java/com/tele/goldenkey/spi/agora/RtcTokenBuilderSample.java
@@ -2,12 +2,6 @@ package com.tele.goldenkey.spi.agora;
 
 
 import com.tele.goldenkey.spi.agora.media.RtcTokenBuilder;
-import com.tele.goldenkey.util.SpringContextUtil;
-import org.apache.commons.lang3.StringUtils;
-import org.redisson.api.RMapCache;
-import org.redisson.api.RedissonClient;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * 视频通话
@@ -15,18 +9,10 @@ import java.util.concurrent.TimeUnit;
 public class RtcTokenBuilderSample {
     private final static String appId = "a75d7dfc56454049aa425f39b085db94";
     private final static String appCertificate = "a0a62266ac204e9eae02add460fabcbd";
-    private final static RedissonClient redissonClient = SpringContextUtil.getBean(RedissonClient.class);
 
     public static String buildToken(String groupId, String userId, RtcTokenBuilder.Role role) {
-        String key = groupId + "_" + userId;
-        RMapCache<String, String> mapCache = redissonClient.getMapCache("tele.goldenkey:agora:rtc");
-        String token = mapCache.get(key);
-        if (StringUtils.isNotBlank(token)) return token;
-
-        RtcTokenBuilder tokenBuilder = new RtcTokenBuilder();
-        token = tokenBuilder.buildTokenWithUserAccount(appId, appCertificate,
-                groupId, userId, role, 0);
-        mapCache.put(key, token, 23, TimeUnit.HOURS);
-        return token;
+        return new RtcTokenBuilder()
+                .buildTokenWithUserAccount(appId, appCertificate,
+                        groupId, userId, role, 0);
     }
 }
