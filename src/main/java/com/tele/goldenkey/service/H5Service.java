ca973c4e79a7cc89e5c8891e206005d797c92acf
lihongbin <836559525@qq.com>
2021-04-22 18:06:12 +0800

    share

--- src/main/java/com/tele/goldenkey/service/H5Service.java
+++ src/main/java/com/tele/goldenkey/service/H5Service.java
@@ -1,9 +1,9 @@
 package com.tele.goldenkey.service;
 
 import com.tele.goldenkey.dao.GroupsMapper;
-import com.tele.goldenkey.dao.LiveUserMapper;
+import com.tele.goldenkey.dao.UsersMapper;
 import com.tele.goldenkey.domain.Groups;
-import com.tele.goldenkey.domain.LiveUser;
+import com.tele.goldenkey.domain.Users;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -19,20 +19,20 @@ public class H5Service {
     private final static String INNER_TXT = "%s邀请您加入%s群聊";
     private final static String SHARE_TXT = "%s邀请你试玩TelePathy,赶快来下载";
 
-    private final LiveUserMapper liveUserMapper;
+    private final UsersMapper usersMapper;
     private final GroupsMapper groupsMapper;
 
 
     public String query(Integer userId, Integer groupId) {
         log.info("userId:{},groupId:{}", userId, groupId);
         if (userId == null) return "";
-        LiveUser liveUser = liveUserMapper.selectByUserId(userId);
-        if (liveUser == null) return "";
+        Users users = usersMapper.selectByPrimaryKey(userId);
+        if (users == null) return "";
         if (groupId == null) {
-            return String.format(SHARE_TXT, liveUser.getName());
+            return String.format(SHARE_TXT, users.getNickname());
         }
         Groups groups = groupsMapper.selectByPrimaryKey(groupId);
         if (groups == null) return "";
-        return String.format(INNER_TXT, liveUser.getName(), groups.getName());
+        return String.format(INNER_TXT, users.getNickname(), groups.getName());
     }
 }
