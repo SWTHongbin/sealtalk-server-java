ecc56897ba47f8269b6e17f8fa5651ce4684a5d9
lihongbin <836559525@qq.com>
2021-06-06 21:59:45 +0800

    test

--- src/main/java/com/tele/goldenkey/controller/param/OptionPriceDto.java
+++ src/main/java/com/tele/goldenkey/controller/param/OptionPriceDto.java
@@ -1,6 +1,5 @@
 package com.tele.goldenkey.controller.param;
 
-import com.sun.istack.internal.NotNull;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -11,6 +10,5 @@ public class OptionPriceDto {
     /**
      * 数值  单位秒
      */
-    @NotNull
     private BigDecimal second;
 }
