d98a4db7c69025df0bf78ff03d40aabad67c3ad3
lihongbin <836559525@qq.com>
2021-06-15 16:37:32 +0800

    testok

--- src/main/java/com/tele/goldenkey/configuration/AppConfiguration.java
+++ src/main/java/com/tele/goldenkey/configuration/AppConfiguration.java
@@ -1,13 +1,38 @@
 package com.tele.goldenkey.configuration;
 
 import com.github.pagehelper.PageHelper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.springframework.context.annotation.*;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Collections;
 import java.util.Properties;
 
 /**
@@ -16,19 +41,27 @@ import java.util.Properties;
  * @Description:
  * @Copyright (c) 2020, rongcloud.cn All Rights Reserved
  */
+@Slf4j
 @Configuration
 @Import({SingleDataSourceConfiguration.class})
 @ComponentScans({@ComponentScan("com.tele.goldenkey.*")})
 public class AppConfiguration {
 
-    public static final int TIMEOUT = 10000;
+    private static final int DEFAULT_TIMEOUT = 10 * 1000;
+
 
     @Bean
-    public RestTemplate restTemplate() {
-        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-        requestFactory.setConnectTimeout(TIMEOUT);
-        requestFactory.setReadTimeout(TIMEOUT);
-        return new RestTemplate(requestFactory);
+    public RestTemplate restTemplate() throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+        CloseableHttpClient httpClient = acceptsUntrustedCertsHttpClient();
+        RestTemplate restTemplate = new RestTemplate(new BufferingClientHttpRequestFactory(new HttpComponentsClientHttpRequestFactory(httpClient)));
+        restTemplate.setInterceptors(Collections.singletonList((request, body, execution) -> {
+            String tradeId = Thread.currentThread().getName().concat("_") + Thread.currentThread().getId();
+            traceRequest(tradeId, request, body);
+            ClientHttpResponse response = execution.execute(request, body);
+            traceResponse(tradeId, response);
+            return response;
+        }));
+        return restTemplate;
     }
 
     @Bean(destroyMethod = "shutdown")
@@ -37,6 +70,7 @@ public class AppConfiguration {
         config.useSingleServer().setAddress("34.94.185.132:19567").setPassword("123654");
         return Redisson.create(config);
     }
+
     /**
      * 分页插件配置
      *
@@ -44,7 +78,6 @@ public class AppConfiguration {
      */
     @Bean
     public PageHelper pageHelper() {
-        System.out.println("开始配置数据分页插件");
         PageHelper pageHelper = new PageHelper();
         Properties properties = new Properties();
         properties.setProperty("rowBoundsWithCount", "true");
@@ -53,5 +86,56 @@ public class AppConfiguration {
         properties.setProperty("dialect", "mysql");
         pageHelper.setProperties(properties);
         return pageHelper;
+    }
+
+    private void traceRequest(String tradeId, HttpRequest request, byte[] body) {
+        log.info("===========================request begin================================================");
+        log.info("traceId     : {}", tradeId);
+        log.info("URI         : {}", request.getURI());
+        log.info("Method      : {}", request.getMethod());
+        log.info("Headers     : {}", request.getHeaders());
+        log.info("Request body: {}", new String(body, StandardCharsets.UTF_8));
+        log.info("==========================request end================================================");
+    }
+
+    private void traceResponse(String tradeId, ClientHttpResponse response) throws IOException {
+        StringBuilder inputStringBuilder = new StringBuilder();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(response.getBody(), StandardCharsets.UTF_8));
+        String line = bufferedReader.readLine();
+        while (line != null) {
+            inputStringBuilder.append(line);
+            line = bufferedReader.readLine();
+        }
+        log.info("============================response begin==========================================");
+        log.info("traceId      : {}", tradeId);
+        log.info("Status code  : {}", response.getStatusCode());
+        log.info("Status text  : {}", response.getStatusText());
+        log.info("Headers      : {}", response.getHeaders());
+        log.info("Response body: {}", inputStringBuilder.toString());
+        log.info("=======================response end=================================================");
+    }
+
+    private CloseableHttpClient acceptsUntrustedCertsHttpClient()
+            throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+        HttpClientBuilder builder = HttpClientBuilder.create();
+        RequestConfig.Builder requestConfig = RequestConfig.custom();
+        requestConfig.setConnectTimeout(DEFAULT_TIMEOUT);
+        requestConfig.setConnectionRequestTimeout(DEFAULT_TIMEOUT);
+        requestConfig.setSocketTimeout(DEFAULT_TIMEOUT);
+        builder.setDefaultRequestConfig(requestConfig.build());
+
+        SSLContext sslContext = new SSLContextBuilder().loadTrustMaterial(null, (arg0, arg1) -> true).build();
+        builder.setSSLContext(sslContext);
+
+        HostnameVerifier hostnameVerifier = NoopHostnameVerifier.INSTANCE;
+        SSLConnectionSocketFactory sslSocketFactory = new SSLConnectionSocketFactory(sslContext, hostnameVerifier);
+        Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("http", PlainConnectionSocketFactory.getSocketFactory()).register("https", sslSocketFactory)
+                .build();
+        PoolingHttpClientConnectionManager connMgr = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
+        connMgr.setMaxTotal(200);
+        connMgr.setDefaultMaxPerRoute(100);
+        builder.setConnectionManager(connMgr);
+        return builder.build();
     }
 }
