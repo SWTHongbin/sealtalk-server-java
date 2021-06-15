f1649437a0c74141071533020ebb5059d5a9b527
lihongbin <836559525@qq.com>
2021-06-15 10:42:30 +0800

    goods

--- src/main/java/com/tele/goldenkey/TelePathyApplication.java
+++ src/main/java/com/tele/goldenkey/TelePathyApplication.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import software.amazon.awssdk.core.SdkSystemSetting;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @EnableTransactionManagement
@@ -21,8 +20,6 @@ import tk.mybatis.spring.annotation.MapperScan;
 public class TelePathyApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        System.setProperty(SdkSystemSetting.AWS_ACCESS_KEY_ID.property(), "AKIAU6KHWM6YI5ZPBFVF");
-        System.setProperty(SdkSystemSetting.AWS_SECRET_ACCESS_KEY.property(), "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");
         SpringApplication.run(TelePathyApplication.class, args);
     }
 
