d968de63c4846e798f133f3b84503e5488a11bf0
lihongbin <836559525@qq.com>
2021-06-15 10:41:24 +0800

    goods

--- src/main/java/com/tele/goldenkey/controller/GoodsController.java
+++ src/main/java/com/tele/goldenkey/controller/GoodsController.java
@@ -12,7 +12,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 /**
- * 商品组件
+ * 商品相关
  */
 @RestController
 @RequestMapping("/goods")
