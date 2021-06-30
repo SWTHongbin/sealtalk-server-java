c7100f90e66e6a25038fd05e40671b6b8041f428
lihongbin <836559525@qq.com>
2021-06-30 11:30:11 +0800

    apple pay result

--- src/main/java/com/tele/goldenkey/service/AppleService.java
+++ src/main/java/com/tele/goldenkey/service/AppleService.java
@@ -14,12 +14,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RBucket;
 import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Slf4j
@@ -38,37 +35,36 @@ public class AppleService {
         Optional<PricePackageDto.Price> optional = PricePackageDto.getInstance().findBySkuAndId(skuType, prepareOrderParam.getPackageId());
         ValidateUtils.isTrue(optional.isPresent());
 
-        JSONObject returnJson = getVerifyResult(payload);
-        String inApp = returnJson.getString("in_app");
-        List<HashMap> inApps = JSONObject.parseArray(inApp, HashMap.class);
-        ValidateUtils.isTrue(!CollectionUtils.isEmpty(inApps), "未能获取获取到交易列表");
-        ArrayList<String> transactionIds = new ArrayList<>();
-        for (HashMap app : inApps) {
-            transactionIds.add((String) app.get("transaction_id"));
-        }
+        getVerifyResult(payload);
+        //  String inApp = returnJson.getString("in_app");
+        //  List<HashMap> inApps = JSONObject.parseArray(inApp, HashMap.class);
+        //  ValidateUtils.isTrue(!CollectionUtils.isEmpty(inApps), "未能获取获取到交易列表");
+        //  ArrayList<String> transactionIds = new ArrayList<>();
+        // for (HashMap app : inApps) {
+        //     transactionIds.add((String) app.get("transaction_id"));
+        //   }
         //交易列表包含当前交易，则认为交易成功
-        ValidateUtils.isTrue(transactionIds.contains(orderNo), "当前交易不在交易列表中");
+        //  ValidateUtils.isTrue(transactionIds.contains(orderNo), "当前交易不在交易列表中");
         pricePackageService.recharge(skuType, userId, BigDecimal.valueOf(optional.get().getSecond()));
         bucket.delete();
         return APIResultWrap.ok();
     }
 
-    private JSONObject getVerifyResult(String payload) throws ServiceException {
+    private void getVerifyResult(String payload) throws ServiceException {
         String verifyResult = IosVerifyUtil.buyAppVerify(payload, 1);
         ValidateUtils.notNull(verifyResult, "苹果验证失败,返回数据为空");
         log.info("线上，苹果平台返回JSON:" + verifyResult);
         JSONObject appleReturn = JSONObject.parseObject(verifyResult);
         String states = appleReturn.getString("status");
-        //无数据则沙箱环境验证
-        if ("21007".equals(states)) {
+        if (Objects.equals("21007", states)) {
             verifyResult = IosVerifyUtil.buyAppVerify(payload, 0);
             log.info("沙盒环境，苹果平台返回JSON:" + verifyResult);
             appleReturn = JSONObject.parseObject(verifyResult);
             states = appleReturn.getString("status");
         }
         log.info("苹果平台返回值：appleReturn" + appleReturn);
-        ValidateUtils.isTrue(states.equals("0"), "支付失败，错误码：" + states);
-        String receipt = appleReturn.getString("receipt");
-        return JSONObject.parseObject(receipt);
+        ValidateUtils.isTrue(Objects.equals("0", states), "支付失败，错误码：" + states);
+        //String receipt = appleReturn.getString("receipt");
+        //     JSONObject.parseObject(receipt);
     }
 }
