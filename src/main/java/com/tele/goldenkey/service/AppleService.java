a7ecc40c6286451487ebd731a9886f71c7fffcce
lihongbin <836559525@qq.com>
2021-07-05 09:15:44 +0800

    try lock

--- src/main/java/com/tele/goldenkey/service/AppleService.java
+++ src/main/java/com/tele/goldenkey/service/AppleService.java
@@ -12,8 +12,10 @@ import com.tele.goldenkey.util.ValidateUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RBucket;
+import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.Objects;
@@ -27,18 +29,25 @@ public class AppleService {
     private final RedissonClient redissonClient;
     private final PricePackageService pricePackageService;
 
+    @Transactional
     public APIResult iap(Integer userId, String orderNo, String payload) throws ServiceException {
-        RBucket<PrepareOrderParam> bucket = redissonClient.getBucket("order" + userId + orderNo);
-        ValidateUtils.isTrue(bucket.isExists(), "订单编号不存在");
-        PrepareOrderParam prepareOrderParam = bucket.get();
-        SkuType skuType = SkuType.byCodeOf(prepareOrderParam.getType());
-        Optional<PricePackageDto.Price> optional = PricePackageDto.getInstance().findBySkuAndId(skuType, prepareOrderParam.getPackageId());
-        ValidateUtils.isTrue(optional.isPresent());
-        verifyResult(payload);
-        log.info("订单号:{},userId:{},苹果支付回调:{}", orderNo, userId, prepareOrderParam);
-        pricePackageService.recharge(skuType, userId, BigDecimal.valueOf(optional.get().getSecond()));
-        bucket.delete();
-        return APIResultWrap.ok();
+        RLock lock = redissonClient.getLock("order.lock." + orderNo);
+        ValidateUtils.isTrue(lock.tryLock(), "请稍等");
+        try {
+            RBucket<PrepareOrderParam> bucket = redissonClient.getBucket("order" + userId + orderNo);
+            ValidateUtils.isTrue(bucket.isExists(), "订单编号不存在");
+            PrepareOrderParam prepareOrderParam = bucket.get();
+            SkuType skuType = SkuType.byCodeOf(prepareOrderParam.getType());
+            Optional<PricePackageDto.Price> optional = PricePackageDto.getInstance().findBySkuAndId(skuType, prepareOrderParam.getPackageId());
+            ValidateUtils.isTrue(optional.isPresent());
+            verifyResult(payload);
+            log.info("订单号:{},userId:{},苹果支付回调:{}", orderNo, userId, prepareOrderParam);
+            pricePackageService.recharge(skuType, userId, BigDecimal.valueOf(optional.get().getSecond()));
+            bucket.delete();
+            return APIResultWrap.ok();
+        } finally {
+            lock.unlock();
+        }
     }
 
     private void verifyResult(String payload) throws ServiceException {
@@ -55,7 +64,5 @@ public class AppleService {
         }
         log.info("苹果平台返回值：appleReturn" + appleReturn);
         ValidateUtils.isTrue(Objects.equals("0", states), "支付失败,错误码：" + states);
-        //String receipt = appleReturn.getString("receipt");
-        //     JSONObject.parseObject(receipt);
     }
 }
